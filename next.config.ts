a8114abdce4a28274c5502469e873c0d1da88118
Rattanan Nantiyakul <rattanan@gmail.com>
2026-07-03 11:19:36 +0700

    Clear client hints headers on responses

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Clear-Site-Data",
+            value: "\"clientHints\"",
+          },
+          {
+            key: "Accept-CH",
+            value: "",
+          },
+          {
+            key: "Critical-CH",
+            value: "",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
